0f61009c8fe7179b14a833febb957bd7d2cd0b50
SALVADORPOETA <dsml@live.com.mx>
2023-05-05 03:14:13 -0600

    labels

--- src/components/Login2.jsx
+++ src/components/Login2.jsx
@@ -16,12 +16,12 @@ export default function Login2() {
                 <p className='border shadow-lg hover:shadow-xl px-6 py-2 relative flex items-center'><FcGoogle className='mr-2' /> Google</p>
             </div>
             <div className='flex flex-col mb-4'>
-                <label>Username</label>
-                <input className='border relative bg-gray-100 p-2' type="text" />
+                <label for='username2' >Username</label>
+                <input id='username2' className='border relative bg-gray-100 p-2' type="text" />
             </div>
             <div className='flex flex-col'>
-                <label>Password</label>
-                <input className='border relative bg-gray-100 p-2' type="password" />
+                <label for='password2' >Password</label>
+                <input id='password2' className='border relative bg-gray-100 p-2' type="password" />
             </div>
             <button className='w-full py-3 mt-8 bg-indigo-600 hover:bg-indigo-500 relative text-white'>Sign In</button>
             <p className='flex items-center mt-2'><input className='mr-2' type="checkbox" />Remember Me</p>
